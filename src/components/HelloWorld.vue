e956539cb8bd7e9e4499238ade34d6c89e2ebb22
Ali Kemal Çelenk <alikemalcelenkk@gmail.com>
2021-09-26 18:35:02 +0300

    create nft card

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,8 @@
     <TheLogo />
     <Navigation />
     <Navigation selected />
+    <Box class="nftCard"><NFTCard :nft="nft" /> </Box>
+    <Box class="nftCard"><NFTCard :nft="nft" liked /> </Box>
   </Box>
 </template>
 
@@ -21,6 +23,7 @@ import IconLogo from './Icons/Logo.vue'
 // components
 import TheLogo from './TheLogo.vue'
 import Navigation from './Navigation.vue'
+import NFTCard from './NFTCard.vue'
 
 export default {
   name: 'HelloWorld',
@@ -30,26 +33,42 @@ export default {
     IconFavoriteFill,
     IconLogo,
     TheLogo,
-    Navigation
+    Navigation,
+    NFTCard
   },
   props: {
     msg: {
       type: String,
       default: 'X page'
     }
+  },
+  data: function () {
+    return {
+      nft: {
+        id: 1,
+        url: 'https://lh3.googleusercontent.com/aKTdnYMXij4YcRE7eduuIq8ADj8VACs34YXBSwcFinQNAzc0kGvcQ5jsGciD2TxLKyjIXJkdqfwhQ8zRK5o-mFTMXyEd2LOVeQqTa0Q=w600',
+        name: '#3336',
+        collection: 'Bored Ape Yacht Club',
+        createdBy: 'BoredApeYachtClub',
+        blockchain: 'Ethereum'
+      }
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .hello {
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
   height: 100vh;
 }
 .icon {
   color: var(--c-blue);
+}
+.nftCard {
+  width: 900px;
 }
 </style>
---
+++ src/components/NFTCard.vue
@@ -0,0 +1,114 @@
+<template>
+  <Box class="ntfcard">
+    <Box class="leftContent">
+      <Image :src="nft.url" class="nftImage" />
+    </Box>
+    <Box class="midContent">
+      <Text class="nftName">{{ nft.name }}</Text>
+      <Text class="description"
+        ><Text class="title">Colection:</Text> {{ nft.collection }}
+      </Text>
+      <Text class="description"
+        ><Text class="title">Created by</Text> {{ nft.createdBy }}
+      </Text>
+      <Text class="description"
+        ><Text class="title">Blockchain:</Text> {{ nft.blockchain }}
+      </Text>
+    </Box>
+    <Box class="rightContent">
+      <Button @click="like">
+        <IconFavoriteFill class="favoriteIcon" v-if="isLiked" />
+        <IconFavorite class="favoriteIcon" v-else />
+      </Button>
+    </Box>
+  </Box>
+</template>
+
+<script>
+// elements
+import Box from './Elements/Box'
+import Image from './Elements/Image'
+import Text from './Elements/Text'
+import Button from './Elements/Button'
+
+// icons
+import IconFavorite from './Icons/Favorite.vue'
+import IconFavoriteFill from './Icons/FavoriteFill.vue'
+
+export default {
+  name: 'NFTCard',
+  components: {
+    Box,
+    Image,
+    Text,
+    Button,
+    IconFavorite,
+    IconFavoriteFill
+  },
+  props: {
+    liked: {
+      type: Boolean,
+      default: false
+    },
+    nft: {
+      type: Object
+    }
+  },
+  data: function () {
+    return {
+      isLiked: this.liked
+    }
+  },
+  methods: {
+    like: function () {
+      this.isLiked = !this.isLiked
+    }
+  }
+}
+</script>
+
+<style scoped>
+.ntfcard {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  width: 100%;
+}
+.leftContent {
+  display: flex;
+  background-color: transparent;
+}
+.nftImage {
+  border: 1px solid var(--c-black);
+  border-radius: 10px;
+  width: 158px;
+}
+.midContent {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  margin-left: 10px;
+  margin-right: 10px;
+  word-break: break-all;
+  white-space: pre-wrap;
+}
+.nftName {
+  font-family: SFProDisplay-SemiBold;
+}
+.title {
+  color: var(--c-blue);
+}
+.description {
+  margin-top: 5px;
+}
+.rightContent {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-shrink: 0;
+}
+.favoriteIcon {
+  color: var(--c-blue);
+}
+</style>
